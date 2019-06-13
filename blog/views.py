7d5072a636bcdb0fc2c8dda627890a24a422357f
paradigma_ggg <39859826+AdamGrizzly@users.noreply.github.com>
2019-06-13 21:10:56 +0600

    Add files via upload

--- blog/views.py
+++ blog/views.py
@@ -4,10 +4,6 @@ from .models import *
    
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
-def hots_list(request):
     hots = Hot.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'hots': hots})
-def guest_list(request):
-    geusts = Guest.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'geusts': geusts})
+    guests = Guest.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts, 'hots':hots, 'guests':guests})
