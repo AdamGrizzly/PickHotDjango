f9b4eb230394f6b86471e39338b4818bba74e9bc
paradigma_ggg <39859826+AdamGrizzly@users.noreply.github.com>
2019-06-14 03:22:10 +0600

    Add files via upload

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'mji6x(+mcoos6aue8c-o_4^seb0u=@%ljhhoi3i)vtve!sgpjg'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.149','127.0.0.1', '192.168.0.104', '192.168.8.143']
+ALLOWED_HOSTS = ['192.168.0.149','127.0.0.1', '192.168.0.104', '192.168.0.106']
 
 
 # Application definition
